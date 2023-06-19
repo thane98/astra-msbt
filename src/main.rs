bbae4a6f5a53560fa53e6dcf3662ecadd8703240
thane98 <>
2023-06-19 07:37:13 -0700

    Fix MSBT serializing

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,10 @@ pub fn main() {
     let args = Args::parse();
     match args.command {
         Command::Extract { bundle, output } => {
-            let msbt = MessageBundle::load(&bundle)
+            let script = MessageBundle::load(&bundle)
                 .expect("failed to parse bundle")
-                .take_msbt();
-            let script = astra_formats::parse_msbt_script(&msbt)
-                .expect("failed to extract script from MSBT");
+                .take_script()
+                .expect("failed to parse script from MSBT");
             if let Some(path) = output {
                 std::fs::write(path, script).expect("failed to write script");
             } else {
@@ -48,14 +47,9 @@ pub fn main() {
         }
         Command::Import { script, bundle, output } => {
             let script = std::fs::read_to_string(script).expect("failed to read script");
-            match astra_formats::pack_astra_script(&script) {
-                Ok(msbt) => {
-                    let mut msbt_bundle = MessageBundle::load(&bundle).expect("failed to load output bundle");
-                    msbt_bundle.replace_msbt(msbt);
-                    msbt_bundle.save(output.unwrap_or(bundle)).expect("failed to save output bundle");
-                }
-                Err(err) => err.report(&script),
-            }
+            let mut msbt_bundle = MessageBundle::load(&bundle).expect("failed to load output bundle");
+            msbt_bundle.replace_script(&script).expect("failed to serialize script");
+            msbt_bundle.save(output.unwrap_or(bundle)).expect("failed to save output bundle");
         }
     }
 }
